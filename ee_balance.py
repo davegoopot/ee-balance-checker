b35c9146e3e3c869d56e6155fb47cdc7e74d5b07
Dave Potts <dave@goopot.co.uk>
2015-01-05 22:06:34 +0000

    Basic grab of login page

--- ee_balance.py
+++ ee_balance.py
@@ -2,6 +2,16 @@
     Login to the https://web.orange.co.uk/ site and parse out the current
     balance.
 """
+
+import requests
+
 class EEConnector(object):
-    """Connect to the website and get the raw page"""
-    pass
+    """Connect to the website and get the raw page showing containing the
+       balance information
+    """
+    
+    def _fetch_login_page(self):
+        """Fetch the login page and return the requests response object"""
+        return requests.get('https://web.orange.co.uk/id/signin.php', 
+                            verify='SymantecClass3SecureServerCA-G4.crt')
+        
--- ee_balance_test.py
+++ ee_balance_test.py
@@ -1,7 +1,16 @@
+"""
+    TODO:  Refactor out EEConnector to set up
+"""
+
 import unittest
 from ee_balance import *
+
 
 class EEUnitTests(unittest.TestCase):
     def test_instance_create(self):
         ee = EEConnector()
         
+    def test_fetch_login_page(self):
+        eecon = EEConnector()
+        login_page_response = eecon._fetch_login_page()
+        self.assertEqual(200, login_page_response.status_code)
