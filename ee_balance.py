53f9534012eaf3550a5f4367f6332e0e986c302f
Dave Potts <dave@goopot.co.uk>
2015-01-05 22:12:22 +0000

    Pylint fixes

--- ee_balance.py
+++ ee_balance.py
@@ -9,9 +9,9 @@ class EEConnector(object):
     """Connect to the website and get the raw page showing containing the
        balance information
     """
-    
+
     def _fetch_login_page(self):
         """Fetch the login page and return the requests response object"""
-        return requests.get('https://web.orange.co.uk/id/signin.php', 
+        return requests.get('https://web.orange.co.uk/id/signin.php',
                             verify='SymantecClass3SecureServerCA-G4.crt')
-        
+
