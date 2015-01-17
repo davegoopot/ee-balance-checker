333351a556e73df2b1383f3deb3ea5195e8e87ec
Dave Potts <dave@goopot.co.uk>
2015-01-17 22:38:45 +0000

    Fixed loading the main page by adding certificates

--- ee_balance.py
+++ ee_balance.py
@@ -9,9 +9,21 @@ class EEConnector(object):
     """Connect to the website and get the raw page showing containing the
        balance information
     """
+    
+    def __init__(self, username, password):
+        """Record the authentication data in the format needed to login to ee"""
+        self.authentication_data = {'LOGIN': username, 'PASSWORD': password}
+        self.session = requests.Session()
+        self.session.verify = 'SymantecClass3SecureServerCA-G4.crt'
 
     def _fetch_login_page(self):
         """Fetch the login page and return the requests response object"""
-        return requests.get('https://web.orange.co.uk/id/signin.php',
-                            verify='SymantecClass3SecureServerCA-G4.crt')
+        return self.session.get('https://web.orange.co.uk/id/signin.php')
+        
 
+    def _fetch_balance_page(self):
+        """Fetch the balance page and return the requests response object"""
+        return self.session.post('https://web.orange.co.uk/id/signin.php?rm=StandardSubmit',
+                                data=self.authentication_data,
+                                allow_redirects=True)
+        
--- ee_balance_test.py
+++ ee_balance_test.py
@@ -1,17 +1,17 @@
-"""
-    TODO:  Refactor out EEConnector to set up
-"""
-
 import unittest
 from ee_balance import *
 
 
 class EEUnitTests(unittest.TestCase):
     def setUp(self):
-        self.eecon = EEConnector()
+        self.eecon = EEConnector(username='07583767812', password='xW7QMsMG48')
         
     def test_fetch_login_page(self):
         login_page_response = self.eecon._fetch_login_page()
         self.assertEqual(200, login_page_response.status_code)
         
-    
+    # def test_fetch_balance_page(self):
+        # balance_page_response = self.eecon._fetch_balance_page()
+        # self.assertEqual(200, balance_page_response.status_code)
+
+        
